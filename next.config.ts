8618d76ce4560252e18b77f60376c50b6c90c32a
PiotrSobiecki <piotr.sobiecki@gameswift.io>
2026-01-23 16:33:53 +0100

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "standalone",
-  webpack: (config: any) => {
+  webpack: (config) => {
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
